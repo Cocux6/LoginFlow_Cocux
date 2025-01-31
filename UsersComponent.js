fb4961386e9d0786f6a414e2b6d95d2684227e96
Cocuz <64425828+Cocux6@users.noreply.github.com>
2025-01-31 23:53:22 +0100

    Create UsersComponent.js

---
+++ UsersComponent.js
@@ -0,0 +1,34 @@
+const fs = require("fs")
+
+class UsersComponent {
+  constructor(statePath) {
+    this.users = {}
+    this.statePath = statePath
+    try {
+      this.users = JSON.parse(fs.readFileSync(statePath, "utf-8"))
+    } catch(err) {
+      console.log(err.message)
+      this.serialize()
+    }
+  }
+
+  serialize() {
+    fs.writeFileSync(this.statePath, JSON.stringify(this.users, null, 2))
+  }
+
+  create(data) {
+    const email = data.email
+    const password = data.password
+
+    this.users[email] = { password }
+    this.serialize()
+  }
+
+  login(email, password) {
+    const user = this.users[email]
+    
+    return user && user.password === password
+  }
+}
+
+module.exports = UsersComponent
